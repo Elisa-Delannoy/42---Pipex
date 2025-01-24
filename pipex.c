b2b7730c5c22b4d2ad0c6ebbb4c9d203f1d0ae45
Elisa Delannoy <elisadelannoy10@gmail.com>
2025-01-24 18:28:31 +0100

    outfile to check

--- pipex.c
+++ pipex.c
@@ -95,14 +95,34 @@ void	ft_exe(char **argv, char **env)
 	path = ft_find_path(env);
 	path_cmd = ft_check_cmd(path, cmd);
 	path_opt = ft_check_opt(path_cmd, cmd);
-	printf("path_cmd =%s\n", path_cmd);
-	int i = -1;
-	while (path_opt[++i])
-		printf("path_opt =%s\n", path_opt[i]);
-	execve(path_cmd, path_opt, NULL);
-	ft_printf("free");
+	// printf("path_cmd =%s\n", path_cmd);
+	// int i = -1;
+	// while (path_opt[++i])
+	// 	printf("path_opt =%s\n", path_opt[i]);
+	if (execve(path_cmd, path_opt, NULL) == -1)
+	{
+		perror("error execve");
+		exit(1);
+	}
+	// voir comment faire pour free car fin du programme
 	free(path_cmd);
 	ft_free_split(path_opt);
+}
+
+void	ft_outfile(int *fd_pipe, char **argv)
+{
+	int	fd_file2;
+
+	fd_file2 = open(argv[3], O_WRONLY); /*mettre 4 quand 2 commande puis voir pour plus*/
+	if (fd_file2 == -1)
+	{
+		perror("error opening outfile");
+		exit(1);
+	}
+	fd_pipe[1] = fd_file2;
+	dup2(fd_pipe[1], STDOUT_FILENO);
+	close(fd_file2);
+	// close(fd_pipe[1]);
 }
 
 void	ft_pipe(int fd_file, char **argv, char **env)
@@ -111,15 +131,32 @@ void	ft_pipe(int fd_file, char **argv, char **env)
 	int	id;
 
 	if (pipe(fd_pipe) == -1)
+	{
 		perror("error pipe");
+		exit(1);
+	}
 	id = fork();
 	if (id < 0)
+	{
 		perror("error fork");
+		exit(1);
+	}
 	if (id == 0)
 	{
+		close (fd_pipe[1]);
 		fd_pipe[0] = fd_file;
 		dup2(fd_pipe[0], STDIN_FILENO);
+		close(fd_file);
+		close(fd_pipe[0]);
 		ft_exe(argv, env);
+	}
+	else
+	{
+		close(fd_pipe[0]);
+		ft_outfile(fd_pipe, argv);
+		close(fd_pipe[1]);
+		printf("after outfile\n");
+		// wait(NULL);
 	}
 }
 
@@ -131,7 +168,10 @@ int	main(int argc, char **argv, char **env)
 
 	fd_file = open(argv[1], O_RDONLY);
 	if (fd_file == -1)
-		return (1);
+	{
+		perror("error opening infile");
+		exit(1);
+	}
 	ft_pipe(fd_file, argv, env);
 	// ft_exe(argv, env);
 	return (0);
